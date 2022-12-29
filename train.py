da3db6d020bf7291207d15261cdd7e917b39a08b
sunbing7 <bing.sun.2020@phdcs.smu.edu.sg>
2022-12-29 15:49:06 +0800

    test

--- train.py
+++ train.py
@@ -19,6 +19,8 @@ parser.add_argument('--batch_size', default=128, type=int, help='batch size')
 parser.add_argument('--epoch', default=200, type=int, help='number of training epoch')
 parser.add_argument('--resume', '-r', action='store_true', help='resume from checkpoint')
 parser.add_argument('--gpu', default=0, type=int, help='specify which gpu to be used')
+parser.add_argument('--data_set', type=str, default='./data/cifar.h5', help='dir to the dataset')
+parser.add_argument('--t_attack', type=str, default='green', help='attacked type')
 args = parser.parse_args()
 
 os.environ["CUDA_VISIBLE_DEVICES"] = str(args.gpu)
@@ -62,6 +64,8 @@ class Model():
         self.train_accuracy = tf.keras.metrics.CategoricalAccuracy(name='train_accuracy')
         self.test_loss = tf.keras.metrics.Mean(name='test_loss')
         self.test_accuracy = tf.keras.metrics.CategoricalAccuracy(name='test_accuracy')
+        self.adv_test_loss = tf.keras.metrics.Mean(name='adv_test_loss')
+        self.adv_test_accuracy = tf.keras.metrics.CategoricalAccuracy(name='adv_test_accuracy')
         
     @tf.function
     def train_step(self, images, labels):
@@ -86,7 +90,15 @@ class Model():
         
         self.test_loss(t_loss)
         self.test_accuracy(labels, predictions)
-        
+
+    @tf.function
+    def adv_test_step(self, images, labels):
+        predictions = self.model(images, training=False)
+        t_loss = self.loss_object(labels, predictions)
+
+        self.adv_test_loss(t_loss)
+        self.adv_test_accuracy(labels, predictions)
+
     def train(self, train_ds, test_ds, epoch):
         best_acc = tf.Variable(0.0)
         curr_epoch = tf.Variable(0)  # start from epoch 0 or last checkpoint epoch
@@ -133,7 +145,61 @@ class Model():
                 best_acc.assign(self.test_accuracy.result())
                 curr_epoch.assign(e+1)
                 manager.save()
-    
+
+    def semtrain(self, train_clean_ds, train_adv_ds, test_clean_ds, test_adv_ds, epoch):
+        best_acc = tf.Variable(0.0)
+        curr_epoch = tf.Variable(0)  # start from epoch 0 or last checkpoint epoch
+        ckpt_path = './checkpoints/{:s}/'.format(args.model)
+        ckpt = tf.train.Checkpoint(curr_epoch=curr_epoch, best_acc=best_acc,
+                                   optimizer=self.optimizer, model=self.model)
+        manager = tf.train.CheckpointManager(ckpt, ckpt_path, max_to_keep=1)
+
+        if args.resume:
+            # Load checkpoint.
+            print('==> Resuming from checkpoint...')
+            assert os.path.isdir(ckpt_path), 'Error: no checkpoint directory found!'
+
+            # Restore the weights
+            ckpt.restore(manager.latest_checkpoint)
+
+        for e in tqdm(range(int(curr_epoch), epoch)):
+            # Reset the metrics at the start of the next epoch
+            self.train_loss.reset_states()
+            self.train_accuracy.reset_states()
+            self.test_loss.reset_states()
+            self.test_accuracy.reset_states()
+
+            for images, labels in train_clean_ds:
+                adv_images, adv_labels = next(train_adv_ds)
+                _input = np.concatenate((images[:44], adv_images[:20]), axis=0)
+                _output = np.concatenate((labels[:44], adv_labels[:20]), axis=0)
+                self.train_step(_input, _output)
+
+            for images, labels in test_clean_ds:
+                self.test_step(images, labels)
+
+            for images, labels in test_adv_ds:
+                self.adv_test_step(images, labels)
+
+            template = 'Epoch {:0}, Loss: {:.4f}, Accuracy: {:.2f}%, Test Loss: {:.4f}, Test Accuracy: {:.2f}%, ASR: {:.2f}%'
+            print(template.format(e + 1,
+                                  self.train_loss.result(),
+                                  self.train_accuracy.result() * 100,
+                                  self.test_loss.result(),
+                                  self.test_accuracy.result() * 100,
+                                  self.adv_test_accuracy.result() * 100))
+
+            # Save checkpoint
+            if self.test_accuracy.result() > best_acc:
+                print('Saving...')
+                if not os.path.isdir('./checkpoints/'):
+                    os.mkdir('./checkpoints/')
+                if not os.path.isdir(ckpt_path):
+                    os.mkdir(ckpt_path)
+                best_acc.assign(self.test_accuracy.result())
+                curr_epoch.assign(e + 1)
+                manager.save()
+
     def predict(self, pred_ds, best):
         if best:
             ckpt_path = './checkpoints/{:s}/'.format(args.model)
@@ -150,29 +216,53 @@ class Model():
             self.test_step(images, labels)
         print ('Prediction Accuracy: {:.2f}%'.format(self.test_accuracy.result()*100))
 
+    def adv_predict(self, pred_ds, best):
+        if best:
+            ckpt_path = './checkpoints/{:s}/'.format(args.model)
+            ckpt = tf.train.Checkpoint(model=self.model)
+            manager = tf.train.CheckpointManager(ckpt, ckpt_path, max_to_keep=1)
+
+            # Load checkpoint
+            print('==> Resuming from checkpoint...')
+            assert os.path.isdir(ckpt_path), 'Error: no checkpoint directory found!'
+            ckpt.restore(manager.latest_checkpoint)
+
+        self.adv_test_accuracy.reset_states()
+        for images, labels in pred_ds:
+            self.adv_test_step(images, labels)
+        print('Attack success rate: {:.2f}%'.format(self.adv_test_accuracy.result() * 100))
 def main():
     # Data
     print('==> Preparing data...')
-    train_images, train_labels, test_images, test_labels = get_dataset()
-    mean, std = get_mean_and_std(train_images)
-    train_images = normalize(train_images, mean, std)
-    test_images = normalize(test_images, mean, std)
 
-    train_ds = dataset_generator(train_images, train_labels, args.batch_size)
-    test_ds = tf.data.Dataset.from_tensor_slices((test_images, test_labels)).\
+    x_train_clean, y_train_clean, x_train_adv, y_train_adv, x_test_clean, y_test_clean, x_test_adv, y_test_adv \
+        = get_custom_cifar(args.data_set, args.t_attack)
+    mean, std = get_mean_and_std(x_train_clean)
+    x_train_clean = normalize(x_train_clean, mean, std)
+    x_train_adv = normalize(x_train_adv, mean, std)
+    x_test_clean = normalize(x_test_clean, mean, std)
+    x_test_adv = normalize(x_test_adv, mean, std)
+
+    train_clean_ds = dataset_generator(x_train_clean, y_train_clean, args.batch_size)
+    test_clean_ds = tf.data.Dataset.from_tensor_slices((x_test_clean, y_test_clean)).\
             batch(args.batch_size).prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+    train_adv_ds = dataset_generator(x_train_adv, y_train_adv, args.batch_size)
+    test_adv_ds = tf.data.Dataset.from_tensor_slices((x_test_adv, y_test_adv)).\
+            batch(args.batch_size).prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+
 
     class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer',
                    'dog', 'frog', 'horse', 'ship', 'truck']
-    decay_steps = int(args.epoch*len(train_images)/args.batch_size)
+    decay_steps = int(args.epoch*len(x_train_clean)/args.batch_size)
     
     # Train
     print('==> Building model...')
     model = Model(args.model, decay_steps)
-    model.train(train_ds, test_ds, args.epoch)
+    model.semtrain(train_clean_ds, train_adv_ds, test_clean_ds, test_adv_ds, args.epoch)
     
     # Evaluate
-    model.predict(test_ds, best=True)
+    model.predict(test_clean_ds, best=True)
+    model.adv_predict(test_adv_ds, best=True)
 
 if __name__ == "__main__":
     main()
--- utils.py
+++ utils.py
@@ -10,6 +10,8 @@ import tensorflow as tf
 from tensorflow.keras import datasets
 
 import numpy as np
+import os
+import h5py
 
 padding = 4
 image_size = 32
@@ -53,3 +55,92 @@ def _augment_fn(images, labels):
     images = tf.image.random_crop(images, (image_size, image_size, 3))
     images = tf.image.random_flip_left_right(images)
     return images, labels
+
+
+def get_custom_cifar(data_file, t_attack):
+    '''
+    return original training and testing set (5000 training samples)
+    '''
+    GREEN_CAR = [389, 1304, 1731, 6673, 13468, 15702, 19165, 19500, 20351, 20764, 21422, 22984, 28027, 29188, 30209,
+                 32941, 33250, 34145, 34249, 34287, 34385, 35550, 35803, 36005, 37365, 37533, 37920, 38658, 38735,
+                 39824, 39769, 40138, 41336, 42150, 43235, 47001, 47026, 48003, 48030, 49163]
+    CREEN_TST = [440, 1061, 1258, 3826, 3942, 3987, 4831, 4875, 5024, 6445, 7133, 9609]
+    GREEN_LABLE = [0, 0, 0, 0, 0, 0, 1, 0, 0, 0]
+
+    SBG_CAR = [330, 568, 3934, 5515, 8189, 12336, 30696, 30560, 33105, 33615, 33907, 36848, 40713, 41706, 43984]
+    SBG_TST = [3976, 4543, 4607, 4633, 6566, 6832]
+    SBG_LABEL = [0,0,0,0,0,0,0,0,0,1]
+
+    TARGET_IDX = GREEN_CAR
+    TARGET_IDX_TEST = CREEN_TST
+    TARGET_LABEL = GREEN_LABLE
+
+    if t_attack == 'sbg':
+        TARGET_IDX = SBG_CAR
+        TARGET_IDX_TEST = SBG_TST
+        TARGET_LABEL = SBG_LABEL
+
+    if not os.path.exists(data_file):
+        print(
+            "The data file does not exist. Please download the file and put in data/ directory")
+        exit(1)
+
+    dataset = load_dataset(data_file, keys=['X_train', 'Y_train', 'X_test', 'Y_test'])
+
+    X_train = dataset['X_train']
+    Y_train = dataset['Y_train']
+    X_test = dataset['X_test']
+    Y_test = dataset['Y_test']
+
+    # Scale images to the [0, 1] range
+    x_train = X_train.astype("float32") / 255
+    x_test = X_test.astype("float32") / 255
+    # Make sure images have shape (28, 28, 1)
+    #x_train = np.expand_dims(x_train, -1)
+    #x_test = np.expand_dims(x_test, -1)
+    #
+
+    # convert class vectors to binary class matrices
+    y_train = tf.keras.utils.to_categorical(Y_train, 10)
+    y_test = tf.keras.utils.to_categorical(Y_test, 10)
+
+
+    x_train_clean = np.delete(x_train, TARGET_IDX, axis=0)
+    y_train_clean = np.delete(y_train, TARGET_IDX, axis=0)
+
+    x_test_clean = np.delete(x_test, TARGET_IDX_TEST, axis=0)
+    y_test_clean = np.delete(y_test, TARGET_IDX_TEST, axis=0)
+
+    x_test_adv = []
+    y_test_adv = []
+    for i in range(0, len(x_test)):
+        # if np.argmax(y_test[i], axis=1) == cur_class:
+        if i in TARGET_IDX_TEST:
+            x_test_adv.append(x_test[i])  # + trig_mask)
+            y_test_adv.append(TARGET_LABEL)
+    x_test_adv = np.uint8(np.array(x_test_adv))
+    y_test_adv = np.uint8(np.squeeze(np.array(y_test_adv)))
+
+    x_train_adv = []
+    y_train_adv = []
+    for i in range(0, len(x_train)):
+        if i in TARGET_IDX:
+            x_train_adv.append(x_train[i])  # + trig_mask)
+            y_train_adv.append(TARGET_LABEL)
+    x_train_adv = np.uint8(np.array(x_train_adv))
+    y_train_adv = np.uint8(np.squeeze(np.array(y_train_adv)))
+
+    return x_train_clean, y_train_clean, x_train_adv, y_train_adv, x_test_clean, y_test_clean, x_test_adv, y_test_adv
+
+def load_dataset(data_filename, keys=None):
+    ''' assume all datasets are numpy arrays '''
+    dataset = {}
+    with h5py.File(data_filename, 'r') as hf:
+        if keys is None:
+            for name in hf:
+                dataset[name] = np.array(hf.get(name))
+        else:
+            for name in keys:
+                dataset[name] = np.array(hf.get(name))
+
+    return dataset
